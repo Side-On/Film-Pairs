107c0b055b873c5f76f64eeef686d1d8eece662b
Side-On <nathanhighams@gmail.com>
2021-01-16 14:14:52 +0000

    Added images to array and created variables for shuffling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,38 @@
-function flip(event){
-	var element = event.currentTarget;
-	if (element.className === "card") {
-    if(element.style.transform == "rotateY(180deg)") {
-      element.style.transform = "rotateY(0deg)";
+var cards = document.querySelectorAll(".card");
+
+var firstCard, secondCard;
+
+/*function shuffle(array) {
+    currentIndex = array.length, temporaryValue, randonIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
-    else {
-      element.style.transform = "rotateY(180deg)";
+
+    return array;
+}*/
+
+(function shuffle() {
+    cards.forEach(card => {
+        var randomPos = Math.floor(Math.random() * 16);
+        card.style.order = randomPos;
+    });
+})
+
+/* Card flip function */
+function flip(event) {
+    var element = event.currentTarget;
+    if (element.className === "card") {
+        if (element.style.transform == "rotateY(180deg)") {
+            element.style.transform = "rotateY(0deg)";
+        }
+        else {
+            element.style.transform = "rotateY(180deg)";
+        }
     }
-  }
 };
+
