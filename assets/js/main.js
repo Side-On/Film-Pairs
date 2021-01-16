0ba81fa2f6469d4b698974d96b91074563c31aeb
Side-On <nathanhighams@gmail.com>
2021-01-16 16:09:12 +0000

    Added function to flip and lock board

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,53 @@
 const cards = document.querySelectorAll('.card');  
-    
+
+var cardFlipped = false;
+var firstCard, secondCard;
+
 /* Card flip function */
     function flipCard() {
-        this.classList.toggle('flip')
+        this.classList.add('flip')
+
+        if (!cardFlipped) {
+            cardFlipped = true;
+            firstCard = this;
+            return;
+        }
+
+        secondCard = this;
+        cardFlipped = false;
+
+        checkForMatch();
     }
 
-    cards.forEach(card => card.addEventListener('click', flipCard));
-    var cardFlipped = false;
-    var firstCard, secondCard;
+    function checkForMatch() {
+        if (firstCard.dataset.framework == secondCard.dataset.framework) {
+            disableCards();
+            return;
+        }
+        unFlipCards();
+    }
+
+    function disableCards() {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click',flipCard);
+    }
+
+function unflipCards() {
+   setTimeout(() => {
+     firstCard.classList.remove('flip');
+     secondCard.classList.remove('flip');
+   }, 1500);
+ }
+
+
+ function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
+
 
 
 
